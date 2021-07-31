de6eda53b29d2bc9493b006d188352d331b3d1c1
Alan Grover <awgrover@gmail.com>
2021-07-31 13:11:23 -0400

    building up initial app

---
+++ cleft_arduino/AccelStepperShift.h
@@ -0,0 +1,387 @@
+#pragma once
+
+#include <SPI.h>
+
+#include <AccelStepper.h> // Mike McCauley <mikem@airspayce.com>
+#include <Streaming.h>
+
+#include "freememory.h"
+#include "every.h"
+
+// true to print "done" everytime all motors finish
+// probably too noisy when running a real pattern
+#define DEBUGDONERUN 1
+
+// the COUNT of motors to debug.
+//  undef or -1 is don't,
+//#define DEBUGBITVECTOR 1
+// 1 to print the bit-vector every time
+// 2 to print on "blink"
+//#define DEBUGLOGBITVECTOR 2
+
+#ifndef DEBUGDONERUN
+#define DEBUGDONERUN 0
+#endif
+#ifndef DEBUGBITVECTOR
+#define DEBUGBITVECTOR -1
+#endif
+#ifndef DEBUGLOGBITVECTOR
+#define DEBUGLOGBITVECTOR 0
+#endif
+
+class AccelStepperNoted: public AccelStepper {
+    // This class just notes that a step is requested.
+    // A user of the instance should reset `do_step` once it is read
+    // Need to call the usual .runX(), and other .setX() as normal
+
+  public:
+    inline boolean direction() {
+      return _direction;  // expose, true == foward
+    }
+    boolean do_step; // true == do a step, reset after this is used
+
+    AccelStepperNoted() : AccelStepper(&dumyStep, &dumyStep) , do_step(false) {}
+    static void dumyStep() {}
+
+    void step(long astep) {
+      // step is just current position, not relevant for 1-pin mode
+      // we just note that a step is requested
+
+      (void)(astep); // Unused
+
+      do_step = true;
+    }
+} ;
+
+class AccelStepperShift {
+    // for many DFRobot TB6600
+    // via a chain of shift-registers (2 bits per tb6600, 2 bits unused, common enable)
+    //  74HC595
+    //  Wiring: Arduino   Shift-Register
+    //                    VCC 3V
+    //                    GND
+    //          MOSI      14 SER
+    //          SCK       11 SRCLK
+    //          latch_pin 12 RCLK
+    //                    13 ~OE pull low
+    //                    10 ~SRCLR pull high
+    //  5MHz clock max at 2v, 25MHz at 5v, so... at 3v...
+    //  20mA max per output (?)
+    // using an AccelStepper per motor
+    //
+    // This class
+    // * constructs AccelStepperNoted per motor
+    // * has run() to run all motors
+    // * converts the "noted" steps into bit-vectors for fast spi-shift out
+    //    to drive the tb6600's
+
+    // driver chip: https://toshiba.semicon-storage.com/info/docget.jsp?did=14683&prodName=TB6600HG
+    //"common anode"
+    // pull up to logic level: PUL+, DIR+ and EN+
+    // 8-15mA each signal
+    // signals: pul-, dir-, en-
+    // (swap those sets for "common cathode" and reverse signal polarities)
+    // That's "1 pin" mode for AccelStepper
+    // min 2.2micros for pulse width
+    // Order:
+    //  set direction & enable ~20micros
+    //    also serves as >2.2micros between pulses
+    //  set pulse ~20micros
+    //  2.2micros
+    //  reset pulse ~20micros
+    //  total ~60microsec
+
+  public:
+    // per frame
+    // invert if necessary
+    static byte constexpr FWD_DIRBIT = 0b10;
+    static byte constexpr REV_DIRBIT = 0b00;
+    static byte constexpr STEPBIT = 0b01;
+    static boolean constexpr LATCHSTART = 1; // 1 if the latch pulse is LOW->HIGH->LOW
+    static boolean constexpr LATCHIDLE = ! LATCHSTART;
+
+    // We are using 8 bit shift registers
+    // And 4 bits per motor (a frame)
+    // We shift out LSB 1st,
+    // so our bit-vector[0] is the first shifted out (farthest shift-register. lsb)
+    // and shift-register[i].bit[7] is our frame-bit[0] ?
+    // so,
+    //  arduino
+    //  -> ...
+    //  -> motor[1]:motor[0]
+    //  -> extra_frame[1]:extra_frame[0]
+    //    led-bar goes here
+    //  -> unused_frame[1]:unused_frame[0]... if any
+    //  end-of-chain
+    static constexpr int bits_per_frame = 4; // dir-,pul-, 2 unused ("en" is common)
+    static constexpr int extra_frames = 8 / bits_per_frame; // the led-bar: 8 leds
+    // cf unused_frames. use frame_i = unused_frames as index of 1st "extra frame"
+    static constexpr int used_bits = 2; // dir-,pul-, 2 unused ("en" is common)
+    static constexpr byte frame_mask = (1 << bits_per_frame) - 1; // just the bits of the frame, i.e. n bits
+    static constexpr byte used_mask = (1 << used_bits) - 1; // just the bits of the frame that are used, i.e. step & dir
+
+    // State
+    // NB: this MUST be in the same order as the initialization list of the constructor
+    // or values can get corrupted.
+    // Turn on "more" warnings in preferences.
+
+    const int motor_ct;
+    const int latch_pin;
+    // derived
+    const int total_bits;
+    const int byte_ct;
+    const int unused_frames; // lsb frames that aren't used (because we need byte aligned)
+
+    AccelStepperNoted** motors; // an [] of them
+
+    // frame order is lsb:
+    // unused-frames, led-bar, motor0 ... motorN
+    // an [] of bytes for the shift-register bits, for per-motor-frame
+    byte* dir_bit_vector = NULL; // dir & step=0 for each frame
+    byte* step_bit_vector = NULL; // dir&step for each frame
+
+    AccelStepperShift(const int motor_ct, const int latch_pin)
+      : motor_ct(motor_ct)
+      , latch_pin(latch_pin)
+      , total_bits( (motor_ct + extra_frames) * bits_per_frame)
+        // we have to fill out the bits to make a whole frame, i.e. ceiling()
+      , byte_ct( ceil( total_bits / (sizeof(byte) * 8.0) ) )
+        // and fill out to byte align
+      , unused_frames( byte_ct - (total_bits / (sizeof(byte) * 8)) )
+    {
+      // not constructing `new` members till .begin()
+    }
+
+    void begin() {
+      pinMode(latch_pin, OUTPUT);
+      digitalWrite(latch_pin, LATCHIDLE);
+
+      // construct now, so we can control when memory is allocated
+      Serial << F("SETUP AccelStepperShift ") << motor_ct
+             << F(" bit_vector bytes ") << byte_ct << F(" bits ") << total_bits << F(" unused frames ") << unused_frames
+             << endl;
+      // I was getting corrupted data because initialization-list must be in same order as instance-vars.
+      if ( unused_frames != (byte_ct - (total_bits  / ((int)sizeof(byte) * 8) ))) {
+        Serial << F("Bad unused_frames again! free ") << freeMemory() << endl;
+        while (1) delay(20);
+      }
+      if (DEBUGBITVECTOR > 0) {
+        Serial
+            << F("(sizeof(byte) * 8.0) ") << (sizeof(byte) * 8.0) << endl
+            << F("byte_ct - (total_bits  / (sizeof(byte) * 8) ") << (byte_ct - (total_bits  / (sizeof(byte) * 8) )) << endl
+            ;
+        Serial << F("Free ") << freeMemory() << endl;
+        //while (1) delay(20);
+      }
+
+      motors = new AccelStepperNoted*[motor_ct];
+      for (int i = 0; i < motor_ct; i++) {
+        motors[i] = new AccelStepperNoted;
+      }
+
+      // allocate the bit-vector
+      // better be 8 bits/byte!
+
+      step_bit_vector = new byte[byte_ct];
+      memset(step_bit_vector, 0, sizeof(byte)*motor_ct);
+      dir_bit_vector = new byte[byte_ct];
+      memset(dir_bit_vector, 0, sizeof(byte)*motor_ct);
+
+      Serial << F("BEGIN AccelStepperShift: ") << endl;
+
+      for (int i = 0; i < motor_ct; i++) {
+        // to 200 steps/sec in 0.1 sec
+        // targetspeed = 1/2 * A * 0.1
+        // (targetspeed*2)/0.1
+        constexpr int max_speed = 200;
+        constexpr float time_to_max = 0.1;
+        constexpr int acceleration = (max_speed * 2) / time_to_max;
+        motors[i]->setAcceleration(acceleration);
+        motors[i]->setMaxSpeed(max_speed); // might be limited by maximum loop speed
+      }
+
+      // setup SPI. not nice. should be global in main setup()
+      SPI.begin();
+    }
+
+    boolean run() {
+      if ( run_all() ) {
+        set_led_bar();
+        shift_out();
+        return true;
+      }
+      return false;
+    }
+
+    void set_led_bar() {
+      // use led-bar to indicate that we are shifting, i.e. running
+
+      static Every::Toggle shift_blink(100); // "blink" one of the leds
+
+      byte dir_bit = 0;
+      byte step_bit = 0;
+      // last bit always on: "running"
+      dir_bit |= 1;
+      step_bit |= 1;
+      // second bit 2/3 on: "dir/step alternating"
+      dir_bit |= 1 << 1;
+      step_bit |= 0 << 1;
+      // third bit 1/3 on: "dir/step alternating"
+      dir_bit |= 0 << 2;
+      step_bit |= 1 << 2;
+      // 4th bit blinking
+      boolean do_blink = shift_blink(); // cache
+      if (do_blink) {
+        dir_bit |= shift_blink.state << 3;
+        step_bit |= shift_blink.state << 3;
+      }
+      set_frame( dir_bit_vector, unused_frames + 0, frame_mask, dir_bit );
+      set_frame( step_bit_vector, unused_frames + 0, frame_mask, step_bit );
+
+      if (do_blink && DEBUGLOGBITVECTOR == 2) {
+        Serial << F("OUT: ") << endl;
+        dump_bit_vectors();
+        //while (1) {};
+      }
+    }
+
+    boolean run_all() {
+      // run all
+      // capture bit vector
+      // return true if ANY ran (have to shift all bits)
+      // We should be overwriting the whole bit-vector buffer(s), all bits
+
+      static boolean all_done = false; // for debug messages
+
+      boolean done = true;
+      for (int i = 0; i < motor_ct; i++) {
+        // ->run is about 4000 micros for 15 motors @ 8MHz clock
+        if ( motors[i]->run() ) {
+          done = false;
+        }
+        else {
+          // if (!all_done) Serial << F("  done ") << i << endl; // message as each motor finishes
+        }
+
+        if (false && DEBUGBITVECTOR > 0 && DEBUGBITVECTOR <= i && !all_done) {
+          Serial
+              << F("motor[") << i << F("] do_step? ") << motors[i]->do_step
+              << endl;
+        }
+
+        if (motors[i]->do_step) {
+          // collect the bits
+          // about 100micros for all 15 at 8MHz 32u4
+          
+          motors[i]->do_step = false; // reset once read
+
+          int frame_i = extra_frames + unused_frames ;
+          frame_i += i;
+
+          if (DEBUGBITVECTOR > 0 && DEBUGBITVECTOR >= i) {
+            Serial << F("BV motor[") << i << F("] ")
+                   << F(" extra ") << extra_frames << F(" unused ") << unused_frames << F(" = ") << (extra_frames + unused_frames)
+                   << F("| frame[") << frame_i << F("] ")
+                   << F(" direction ") << motors[i]->direction()
+                   << endl;
+            Serial << F("  mask 0b") << _BIN(frame_mask) << endl;
+            //while (1) delay(20);
+          }
+
+          // want the dir-bit, and !step
+          const byte dir_bit = ((motors[i]->direction() ? FWD_DIRBIT : REV_DIRBIT) | (~STEPBIT & used_mask));
+          // want the dir-bit AND step
+          const byte step_bit = ((motors[i]->direction() ? FWD_DIRBIT : REV_DIRBIT) | STEPBIT);
+
+          if (DEBUGBITVECTOR > 0 && DEBUGBITVECTOR >= i) {
+            Serial << "  new dir_bits " << _BIN(dir_bit) << endl;
+            Serial << "  new step_bits " << _BIN(step_bit) << endl;
+            //while (1) delay(20);
+          }
+
+          int byte_i = set_frame( dir_bit_vector, frame_i, frame_mask, dir_bit );
+          set_frame( step_bit_vector, frame_i, frame_mask, step_bit );
+
+          if (DEBUGBITVECTOR > 0 && DEBUGBITVECTOR >= i) {
+            Serial << F("  dir byte[") << byte_i << F("]= ") << _BIN(dir_bit_vector[ byte_i ]) << endl;
+            Serial << F("  step byte[") << byte_i << F("]= ") << _BIN(step_bit_vector[ byte_i ]) << endl;
+          }
+
+          // dump, then stop if debugging this motor
+          if (DEBUGLOGBITVECTOR == 1 || (DEBUGBITVECTOR > 0 && DEBUGBITVECTOR == i)) {
+            for (int bi = byte_ct - 1; bi >= 0; bi--) {
+              Serial << F("      ") << F(" ") << (bi > 10 ? "" : " ") << bi << F(" ");
+            }
+            Serial << endl;
+
+            dump_bit_vectors();
+            if (DEBUGBITVECTOR > 0 && DEBUGBITVECTOR == i) while (1) delay(20);
+          }
+        }
+
+      }
+      if (DEBUGDONERUN && (!all_done && done)) Serial << F("All done @ ") << millis() << endl;
+      all_done = done;
+
+      return ! done;
+    }
+
+    void dump_bit_vectors() {
+      // just for debug print
+      byte* twobvs[2] = { dir_bit_vector, step_bit_vector };
+      for (byte* bv : twobvs) {
+        for (int bi = byte_ct - 1; bi >= 0; bi--) {
+          for (int bit_i = 0; bit_i < 8; bit_i++) {
+            if ( ! (bit_i % bits_per_frame) && bit_i != 0) Serial << ".";
+            Serial << ( ( bv[bi] & (1 << (7 - bit_i)) ) ? '1' : '0' );
+          }
+          Serial << " ";
+        }
+        Serial << endl;
+      }
+      Serial << endl;
+    }
+
+    void shift_out() {
+      // Send bits,
+      // (NB: dir_bit_vector & step_bit_vector get overwritten)
+      // each is about 60micros at 4MHz spi
+      // at least 2.2micros pulse durations, and each takes 60, so ok
+      // SPI.transfer(byte []) overwrites the byte[] buffer,
+      // and we want to reuse it here.
+      // SO make a copy of dir_bit_vector
+
+      byte dir_copy[byte_ct];
+      memcpy( dir_copy, dir_bit_vector, byte_ct * sizeof(byte));
+
+      // use beginTransaction() to be friendly to other spi users (& disable interrupts!)
+      SPI.beginTransaction(SPISettings(6000000, LSBFIRST, SPI_MODE0));
+
+      SPI.transfer(dir_bit_vector, motor_ct);
+      // latch signal needs to be 100ns long, and digitalWrite takes 5micros! so ok.
+      digitalWrite(latch_pin, LATCHSTART); digitalWrite(latch_pin, LATCHIDLE);
+      SPI.transfer(step_bit_vector, motor_ct);
+      digitalWrite(latch_pin, LATCHSTART); digitalWrite(latch_pin, LATCHIDLE);
+      SPI.transfer(dir_bit_vector, motor_ct); // this is "step pulse off"
+
+      SPI.endTransaction(); // "as soon as possible"
+      // last latch
+      digitalWrite(latch_pin, LATCHSTART); digitalWrite(latch_pin, LATCHIDLE);
+    }
+
+    inline int set_frame( byte * bit_vector, int frame_i, byte mask, byte value ) {
+      // In the bit_vector
+      // at the [frame_i] (lsb)
+      // update the bits to `value`, masked by `mask`
+      // returns the byte_i
+      const int byte_i = (frame_i * bits_per_frame ) / (sizeof(byte) * 8);
+      const int offset = (frame_i * bits_per_frame ) % 8;
+      if (DEBUGBITVECTOR > 0) {
+        Serial << F("    set [") << byte_i << F("] offset ") << offset << F(" mask 0b") << _BIN(mask << offset) << F(" = ") << _BIN(value << offset) << endl;
+      }
+      bit_vector[ byte_i ] = ( bit_vector[ byte_i ] & ~(mask << offset) ) | (value << offset);
+      return byte_i;
+    }
+};
+
+#undef DEBUGBITVECTOR
--- cleft_arduino/cleft_arduino.ino
+++ cleft_arduino/cleft_arduino.ino
@@ -1,36 +1,112 @@
 /*
+  Interactive animation system for Cleft (Annett Couwenberg)
+    cf. http://www.annetcouwenberg.com/spirit
+  1st installation at https://cadvc.umbc.edu/ 2021
 
-Motor shield has 5 i2c address pins: base 0x60 + pin setting.
+  The sculpture has 15 segments, roughly pie-pieces.
+  Each piece has a motor attached to its outer edge,
+  the inner edge being at a fixed height, and fixed to adjacent segments.
+  So, it can tilt.
+
+  Based on a sensor (initially a 8x8 ir camera),
+  the segments should perform various pleasing motions.
+
+  As of July 2021, the design is:
+    one arduino Zero equivalent (48MHz)
+    15 motors, with TB6600 controllers, and limit switches
+    a chain of shift registers driving the controllers (SPI)
+    3 IR cameras (I2C)
+    Several LED indicators
+    AccelStepper to run the motors (but capturing as bit-vector for SPI)
+    An animation framework based on key-frames and transition-functions.
+    An interaction module that interprets the cameras and decides on animation sequences.
+    Lots of wiring & connectors
 
 */
 
-// prefix
-#include "awg_combinators/serial.h"
+#include <AccelStepper.h> // Mike McCauley <mikem@airspayce.com>
+#include <Streaming.h> // Mikal Hart <mikal@arduiniana.org>
+// Not in my version of streaming, for F("") strings:
+Print &operator <<(Print &obj, const __FlashStringHelper* arg) {
+  obj.print(arg);
+  return obj;
+}
 
-constexpr int SEGMENT_CT=7;
-using Heights = float[SEGMENT_CT];
-
-// Our bits
-#include "AccelStepperMotorShield.h"
-#include "ExponentialSmooth.h"
-#include "every.h"
+// NB: a #include list is auto-generated of extant .h files
+//  and it is alphabetical, so this order is not relevant
+//  and, in fact, is redundant for arduino-ide
 #include "freememory.h"
-#include "array_animation.h"
+#include "AccelStepperShift.h"
 
-#include "all_motors.h"
-AllMotors all_motors;
+constexpr int MOTOR_CT = 15;
+constexpr int LATCH_PIN = LED_BUILTIN;
+
+AccelStepperShift* motor_system; // holds AccelSteppers, bit-vector, and shift-out code
 
 void setup() {
-  Serial.begin(115200); while(!Serial);
+  // Integrated USB chips have trouble uploading sometimes
+  // (especially if you corrupt memory, or go into a tight loop)
+  // So, give some time at startup to notice re-upload
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000); // allow uploading when cpu gets screwed
+  digitalWrite(LED_BUILTIN, LOW);
+
+  // We want to note memory
+  const int base_memory = freeMemory();
+  int last_free = base_memory;
+
+  Serial.begin(115200); while (!Serial) {}
+  Serial << endl;
+  Serial << F("Base ") << base_memory << endl;
+  Serial << F("After Serial.begin ") << freeMemory() << endl;
+  Serial << F("Clock ") << ((F_CPU / 1000.0) / 1000.0) << F(" MHz") << endl;
+
+  // Each main object
+
+  motor_system = new AccelStepperShift(MOTOR_CT, LATCH_PIN);
+  Serial << F("AccelStepperShift") << endl;
+  Serial << F("  new")
+         << F(" free ") << freeMemory()
+         << F(" used ") << (last_free - freeMemory()) << endl;
+  last_free = freeMemory();
+  motor_system->begin();
+  Serial << F("  begin")
+         << F(" free ") << freeMemory()
+         << F(" used ") << (last_free - freeMemory()) << endl;
+  last_free = freeMemory();
+}
+
+void loop() {
+  delay(20);
+}
+
+/*
+  // prefix
+  #include "awg_combinators/serial.h"
+
+  constexpr int SEGMENT_CT = 7;
+  using Heights = float[SEGMENT_CT];
+
+  // Our bits
+  #include "AccelStepperMotorShield.h"
+  #include "ExponentialSmooth.h"
+  #include "every.h"
+  #include "freememory.h"
+  #include "array_animation.h"
+
+
+  void setup() {
+  Serial.begin(115200); while (!Serial);
   Serial << F("Startup ") << millis() << F(" free ") << freeMemory() << endl;
 
 
   all_motors.begin();
   Serial << F("Animation ") << millis() << F(" free ") << freeMemory() << endl;
-}
+  }
 
 
-void loop() {
+  void loop() {
   static unsigned long last_time = millis();
   Heights delta;
   static ArrayAnimation anim; // = ArrayAnimation.goto_zero_plane();
@@ -38,35 +114,35 @@ void loop() {
   // note framerate
   static Every say_looprate(500);
   static ExponentialSmooth<unsigned long> framerate(1);
-  static unsigned long last_loop_time=millis();
+  static unsigned long last_loop_time = millis();
   static Every animation_update(1000 / 10); // 10 frames/sec maximum
 
   // update motors every frame
   if ( all_motors.run() || animation_update() ) { // i.e. done-running or time for next frame
     Serial << F("Next step") << endl;
 
-    anim.update( millis() - last_time, delta); 
+    anim.update( millis() - last_time, delta);
     last_time = millis();
     Serial << F("delta "); pheights( delta ); Serial << endl;
-      
-      anim.add( delta );
-      Heights motor_values;
-      //anim.scaled( StepsPerMeter, motor_values);
-    }
+
+    anim.add( delta );
+    Heights motor_values;
+    //anim.scaled( StepsPerMeter, motor_values);
+  }
 
   // info
   framerate.average( millis() - last_loop_time );
   last_loop_time = millis();
   if ( say_looprate() ) {
-    float fr = 1000.0/framerate.value();
+    float fr = 1000.0 / framerate.value();
     Serial << F("looprate ") << fr << F(" free ") << freeMemory() << endl;
   }
 
-}
+  }
 
-void pheights( Heights delta ) {
-  for (int i=0; i<SEGMENT_CT; i++) {
+  void pheights( Heights delta ) {
+  for (int i = 0; i < SEGMENT_CT; i++) {
     Serial << delta[i] << F(" ");
   }
-}
-
+  }
+*/
