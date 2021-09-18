395edd89ce5c3718af7abcc529a4346700dec7e0
Alan Grover <awgrover@gmail.com>
2021-09-18 15:42:29 -0400

    add files

---
+++ arduino/cleft/Animation.h
@@ -0,0 +1,49 @@
+#pragma once
+
+/*
+  A manager/protocol,
+  I plan on an animation framework, so this is an interim solution.
+*/
+
+#include "every.h"
+#include "begin_run.h"
+#include "AccelStepperShift.h"
+
+class Animation : public BeginRun {
+    // Protocol that Animations do.
+    // FIXME: move to own file, or will change w/real animation framework
+    // ALSO: manager for current_anmiation (class static)
+
+  public:
+    // keep track of your own state!
+    // FIXME: document which are public states
+    enum State { Restart, Starting, Running, Stopping, Idle, Off };
+
+    // Which animation we are running
+    static Animation* current_animation;
+    static Animation* animations[];
+    static const int animation_ct;
+
+    AccelStepperShift* const all_motors; // ->motors[] of AccelStepper
+    State state = Restart;
+
+    Animation(AccelStepperShift* all_motors) : all_motors(all_motors) {}
+
+    boolean is_runnning() {
+     return ! ( state == Idle || state == Off);
+    }
+    
+    void mirror_moveTo( int left_i, int amplitude) {
+      // moves motor [left_i] and the mirror'd right side the same
+
+      all_motors->motors[left_i]->moveTo( amplitude );
+      all_motors->motors[ all_motors->motor_ct - left_i - 1 ]->moveTo( amplitude );
+      Serial << F("Move ") << left_i << F( " / " ) << (all_motors->motor_ct - left_i - 1) << F(" TO ") << amplitude << endl;
+    }
+
+  virtual boolean commands(char command) {
+    // handle a serial-input character as a command: return true
+    // return false otherwise (someone else might handle it)
+    return false;
+  }
+};
---
+++ arduino/cleft/AnimationTest.h
@@ -0,0 +1,224 @@
+#pragma once
+
+/*
+  Several test animations
+*/
+
+#include "Animation.h"
+
+class AnimationFast : public Animation {
+    // run real fast
+  public:
+    // for the  smaller motor at 0.5A, 400 is about max (no microstep)
+    static constexpr int speed = 1000; // steps per sec, about fastest I can run
+    static constexpr int time_to_accel = 2; // secs
+
+    AnimationFast(AccelStepperShift* all_motors)
+      : Animation(all_motors)
+    {
+    }
+
+    void restart() {
+      Serial << F("AF start ") << ((long) this) << endl;
+
+      // Want to get up to speed, run for 1 second, then decl
+      int accel = speed / time_to_accel; // accel to speed in 2 second
+      int distance_while_accelerating = accel / 2 * time_to_accel * time_to_accel;
+      int total_distance = 2 * distance_while_accelerating + speed;
+
+      // initial target is the max-amplitude of each segment
+      for (int i = 0; i < all_motors->motor_ct; i++) {
+        all_motors->motors[i]->setMaxSpeed(speed);
+        all_motors->motors[i]->setAcceleration(accel);
+        all_motors->motors[i]->move( total_distance );
+      }
+    }
+
+    void begin() {
+    }
+
+    boolean run() {
+      switch (state) {
+        case Restart:
+          restart();
+          state = Running;
+          break;
+
+        case Starting:
+          startup();
+          return true;
+          break;
+
+        case Running:
+          running();
+          return true;
+          break;
+
+        case Stopping:
+          stopping();
+          return true;
+          break;
+
+        case Idle:
+          break;
+
+        case Off:
+          // don't restart
+          break;
+      }
+      return false; // IFF working
+    }
+
+    void startup() {
+      // Nothing to do, just run
+      Serial << F("FAIL: should do startup") << endl;
+      delay(500);
+    }
+
+    void running() {
+
+      // Nothing to do, just run
+    }
+
+    void stopping() {
+      // we never get here
+    }
+};
+
+class AnimationMotorTests : public Animation {
+    // Takes various commands to run motors in various ways
+    // Type '/' to get commands
+
+  public:
+    // for the  smaller motor at 0.5A, 400 is about max (no microstep)
+    int time_to_accel = 2; // secs
+    int distance = 200; // secs
+    int speed = 200; // steps per sec
+
+    AnimationMotorTests(AccelStepperShift* all_motors)
+      : Animation(all_motors)
+    {
+    }
+
+    void restart() {
+      Serial << F("AF start ") << ((long) this) << endl;
+      allow_random = false; // disable idles while testing
+
+      // Want to get up to speed, run for 1 second, then decl
+      int accel = speed / time_to_accel; // accel to speed in 2 second
+      int distance_while_accelerating = accel / 2 * time_to_accel * time_to_accel;
+      int total_distance = 2 * distance_while_accelerating + speed;
+
+      // initial target is the max-amplitude of each segment
+      for (int i = 0; i < all_motors->motor_ct; i++) {
+        all_motors->motors[i]->setMaxSpeed(speed);
+        all_motors->motors[i]->setAcceleration(accel);
+      }
+      Serial << F("Speed ") << speed
+             << F(" Accel ") << accel
+             << F(" to ") << distance
+             << endl;
+      state = Starting;
+    }
+
+    void begin() {
+    }
+
+    boolean run() {
+      switch (state) {
+        case Restart:
+          restart();
+          break;
+
+        case Starting:
+          startup();
+          break;
+
+        case Running:
+          running();
+          break;
+
+        case Stopping:
+          stopping();
+          break;
+
+        case Idle:
+          break;
+
+        case Off:
+          // don't restart
+          break;
+      }
+      return false; // IFF working
+    }
+
+    void startup() {
+      // Nothing to do, just run
+      for (int i = 0; i < all_motors->motor_ct; i++) {
+        all_motors->motors[i]->move( distance );
+      }
+      Serial << F("+") << endl;
+      state = Running;
+    }
+
+    void running() {
+      if (all_motors->all_done) {
+        for (int i = 0; i < all_motors->motor_ct; i++) {
+          all_motors->motors[i]->move( - distance );
+        }
+        Serial << F("-") << endl;
+        state = Stopping;
+      }
+    }
+
+    void stopping() {
+      if (all_motors->all_done) {
+        Serial << F("!") << endl;
+        state = Idle;
+        Serial << F("Speed ") << speed
+               << F(" to ") << distance
+               << endl;
+      }
+    }
+
+    boolean commands(char command) {
+      boolean handled = false;
+      Serial << F("Try ") << command << endl;
+
+      switch (command) {
+        case '/': // help
+          Serial
+              << F("A  all motors forward/backward slow") << endl
+              << F("B  all motors forward/backward medium") << endl
+              << F("C  all motors forward/backward fast") << endl
+              << F("/  help")
+              << endl;
+          handled = true;
+          break;
+
+        case 'A' :
+          speed = 60;
+          distance = speed * 8;
+          restart();
+          break;
+
+        case 'B' :
+          speed = 200;
+          distance = 200 * 4;
+          restart();
+          break;
+
+        case 'C' :
+          speed = 1000;
+          distance = 200 * 4;
+          restart();
+          break;
+
+        default:
+          Serial << F("???") << endl;
+          break;
+      }
+
+      return handled;
+    }
+};
