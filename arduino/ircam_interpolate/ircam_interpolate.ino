803d3b29c0b49d66699a27b2aa76e784cd839f41
Alan Grover <awgrover@gmail.com>
2021-10-31 15:38:54 -0400

    arduino/ircam_interpolate/

---
+++ arduino/ircam_interpolate/ircam_interpolate.ino
@@ -0,0 +1,175 @@
+/*
+  modified to work with my processing/ir_camera visualizer.
+
+  Tried the adafruit i2c extender (https://www.adafruit.com/product/4756 (LTC4311 based):
+  * did not work: M0 -> extender -> 11' pots wire -> ir cam
+  * did work: M0 -> extender -> 11' pots wire -> muz -> 3" stemma/qwic -> ir cam
+*/
+/***************************************************************************
+  This is a library for the AMG88xx GridEYE 8x8 IR camera
+
+  This sketch makes an inetrpolated pixel thermal camera with the
+  GridEYE sensor and a 2.4" tft featherwing:
+	 https://www.adafruit.com/product/3315
+
+  Designed specifically to work with the Adafruit AMG8833 Featherwing
+          https://www.adafruit.com/product/3622
+
+  These sensors use I2C to communicate. The device's I2C address is 0x69
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit andopen-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Dean Miller, James DeVito & ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+
+
+#include <Wire.h>
+#include <Adafruit_AMG88xx.h>
+#include <Every.h>
+#include <ExponentialSmooth.h>
+#include <Streaming.h>
+#include <SparkFun_I2C_Mux_Arduino_Library.h> //Click here to get the library: http://librarymanager/All#SparkFun_I2C_Mux
+QWIICMUX myMux;
+
+//Comment this out to remove the text overlay
+//#define SHOW_TEMP_TEXT
+
+//low range of the sensor (this will be blue on the screen)
+#define MINTEMP 20
+
+//high range of the sensor (this will be red on the screen)
+#define MAXTEMP 28
+
+//the colors we will be using
+const uint16_t camColors[] = {0x480F,
+                              0x400F, 0x400F, 0x400F, 0x4010, 0x3810, 0x3810, 0x3810, 0x3810, 0x3010, 0x3010,
+                              0x3010, 0x2810, 0x2810, 0x2810, 0x2810, 0x2010, 0x2010, 0x2010, 0x1810, 0x1810,
+                              0x1811, 0x1811, 0x1011, 0x1011, 0x1011, 0x0811, 0x0811, 0x0811, 0x0011, 0x0011,
+                              0x0011, 0x0011, 0x0011, 0x0031, 0x0031, 0x0051, 0x0072, 0x0072, 0x0092, 0x00B2,
+                              0x00B2, 0x00D2, 0x00F2, 0x00F2, 0x0112, 0x0132, 0x0152, 0x0152, 0x0172, 0x0192,
+                              0x0192, 0x01B2, 0x01D2, 0x01F3, 0x01F3, 0x0213, 0x0233, 0x0253, 0x0253, 0x0273,
+                              0x0293, 0x02B3, 0x02D3, 0x02D3, 0x02F3, 0x0313, 0x0333, 0x0333, 0x0353, 0x0373,
+                              0x0394, 0x03B4, 0x03D4, 0x03D4, 0x03F4, 0x0414, 0x0434, 0x0454, 0x0474, 0x0474,
+                              0x0494, 0x04B4, 0x04D4, 0x04F4, 0x0514, 0x0534, 0x0534, 0x0554, 0x0554, 0x0574,
+                              0x0574, 0x0573, 0x0573, 0x0573, 0x0572, 0x0572, 0x0572, 0x0571, 0x0591, 0x0591,
+                              0x0590, 0x0590, 0x058F, 0x058F, 0x058F, 0x058E, 0x05AE, 0x05AE, 0x05AD, 0x05AD,
+                              0x05AD, 0x05AC, 0x05AC, 0x05AB, 0x05CB, 0x05CB, 0x05CA, 0x05CA, 0x05CA, 0x05C9,
+                              0x05C9, 0x05C8, 0x05E8, 0x05E8, 0x05E7, 0x05E7, 0x05E6, 0x05E6, 0x05E6, 0x05E5,
+                              0x05E5, 0x0604, 0x0604, 0x0604, 0x0603, 0x0603, 0x0602, 0x0602, 0x0601, 0x0621,
+                              0x0621, 0x0620, 0x0620, 0x0620, 0x0620, 0x0E20, 0x0E20, 0x0E40, 0x1640, 0x1640,
+                              0x1E40, 0x1E40, 0x2640, 0x2640, 0x2E40, 0x2E60, 0x3660, 0x3660, 0x3E60, 0x3E60,
+                              0x3E60, 0x4660, 0x4660, 0x4E60, 0x4E80, 0x5680, 0x5680, 0x5E80, 0x5E80, 0x6680,
+                              0x6680, 0x6E80, 0x6EA0, 0x76A0, 0x76A0, 0x7EA0, 0x7EA0, 0x86A0, 0x86A0, 0x8EA0,
+                              0x8EC0, 0x96C0, 0x96C0, 0x9EC0, 0x9EC0, 0xA6C0, 0xAEC0, 0xAEC0, 0xB6E0, 0xB6E0,
+                              0xBEE0, 0xBEE0, 0xC6E0, 0xC6E0, 0xCEE0, 0xCEE0, 0xD6E0, 0xD700, 0xDF00, 0xDEE0,
+                              0xDEC0, 0xDEA0, 0xDE80, 0xDE80, 0xE660, 0xE640, 0xE620, 0xE600, 0xE5E0, 0xE5C0,
+                              0xE5A0, 0xE580, 0xE560, 0xE540, 0xE520, 0xE500, 0xE4E0, 0xE4C0, 0xE4A0, 0xE480,
+                              0xE460, 0xEC40, 0xEC20, 0xEC00, 0xEBE0, 0xEBC0, 0xEBA0, 0xEB80, 0xEB60, 0xEB40,
+                              0xEB20, 0xEB00, 0xEAE0, 0xEAC0, 0xEAA0, 0xEA80, 0xEA60, 0xEA40, 0xF220, 0xF200,
+                              0xF1E0, 0xF1C0, 0xF1A0, 0xF180, 0xF160, 0xF140, 0xF100, 0xF0E0, 0xF0C0, 0xF0A0,
+                              0xF080, 0xF060, 0xF040, 0xF020, 0xF800,
+                             };
+
+Adafruit_AMG88xx amg;
+unsigned long delayTime;
+
+#define AMG_COLS 8
+#define AMG_ROWS 8
+float pixels[AMG_COLS * AMG_ROWS];
+
+#define INTERPOLATED_COLS 24
+#define INTERPOLATED_ROWS 24
+
+
+float get_point(float *p, uint8_t rows, uint8_t cols, int8_t x, int8_t y);
+void set_point(float *p, uint8_t rows, uint8_t cols, int8_t x, int8_t y, float f);
+void get_adjacents_1d(float *src, float *dest, uint8_t rows, uint8_t cols, int8_t x, int8_t y);
+void get_adjacents_2d(float *src, float *dest, uint8_t rows, uint8_t cols, int8_t x, int8_t y);
+float cubicInterpolate(float p[], float x);
+float bicubicInterpolate(float p[], float x, float y);
+void interpolate_image(float *src, uint8_t src_rows, uint8_t src_cols,
+                       float *dest, uint8_t dest_rows, uint8_t dest_cols);
+void setup() {
+  while (!Serial) delay(100);
+  Serial.begin(115200);
+  Serial.println("\n\nAMG88xx Interpolated Thermal Camera...");
+
+  //Wire.setClock(100000);
+
+  if (myMux.begin()) {
+    Serial.println("Mux detected");
+    myMux.setPort(1); //Connect master to port labeled '1' on the mux
+    Serial << F("Using port 1 on mux") << endl;
+    byte currentPortNumber = myMux.getPort();
+    Serial.print("CurrentPort: ");
+    Serial.println(currentPortNumber);
+
+  }
+  // default settings
+  if (!amg.begin()) {
+    Serial.println("Could not find a valid AMG88xx sensor, check wiring!");
+    while (1) {
+      delay(1);
+    }
+  }
+
+  Serial.println("-- Thermal Camera Test --");
+  delay(1000);
+}
+
+void loop() {
+  static Every say_size(3 * 1000, true); // fire immediately
+
+  static Every ir_framerate(100);
+  static ExponentialSmooth<unsigned long> cam_rate = ExponentialSmooth<unsigned long>(5);
+  static unsigned long last_loop = millis();
+  static Every say_rate(1000);
+
+  if (say_size()) {
+    Serial << "xy[" << INTERPOLATED_COLS << ", " << INTERPOLATED_ROWS << " ]" << endl;
+  }
+
+  if (ir_framerate()) {
+    // time it
+    last_loop = millis();
+
+    //read all the pixels
+    amg.readPixels(pixels);
+
+    static float dest_2d[INTERPOLATED_ROWS * INTERPOLATED_COLS];
+
+    int32_t t = millis();
+    interpolate_image(pixels, AMG_ROWS, AMG_COLS, dest_2d, INTERPOLATED_ROWS, INTERPOLATED_COLS);
+    Serial.print("Interpolation took "); Serial.print(millis() - t); Serial.println(" ms");
+
+    drawpixels(dest_2d, INTERPOLATED_ROWS, INTERPOLATED_COLS);
+
+  }
+}
+
+void drawpixels(float *p, uint8_t rows, uint8_t cols) {
+  int colorTemp;
+
+  Serial.print("C[");
+  for (int y = 0; y < rows; y++) {
+    for (int x = 0; x < cols; x++) {
+      float val = get_point(p, rows, cols, x, y);
+      if (val >= MAXTEMP) colorTemp = MAXTEMP;
+      else if (val <= MINTEMP) colorTemp = MINTEMP;
+      else colorTemp = val;
+
+      uint8_t colorIndex = map(colorTemp, MINTEMP, MAXTEMP, 0, 255);
+      colorIndex = constrain(colorIndex, 0, 255);
+      //draw the pixels!
+      Serial.print(camColors[colorIndex]);
+      Serial.print(", ");
+
+    }
+    Serial.println();
+
+  }
+  Serial << "]" << endl;
+}
