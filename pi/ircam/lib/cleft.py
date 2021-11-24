a8bc0f79d2222b1058848f8de9d2677432a09426
Alan Grover <awgrover@gmail.com>
2021-11-24 10:48:30 -0500

    minmax, start firsthigh

--- pi/ircam/lib/cleft.py
+++ pi/ircam/lib/cleft.py
@@ -4,40 +4,73 @@ from abc import ABC, abstractmethod
 class AnalyzerInterface:
     """Implement this in analyzers that are Analyze.add( some_analyzer )"""
     @abstractmethod
-    def __call__(x,y,temp):
+    def __call__(self,x,y,temp):
         pass
 
     @abstractmethod
-    def print():
+    def next(self):
+        pass
+
+    @abstractmethod
+    def print_stats(self):
+        pass
+
+    @abstractmethod
+    def post():
+        """do the whole frame analysis"""
         pass
 
 class Analyze:
-    # holds a bunch of analyzers
+    # Analyze data as it comes in: analyzer(x,y,temp)
+    # Analyze the whole frame of data: analyzer()
+    # .print_stats() to print it
+    # Reset for next frame analyzer.next()
+    #
+    # Add .accumulator( new someAnalyzerInterface )
+    # add .post( new somePostInterface )
+
     def __init__(self):
-        self.analyzers = []
+        self.accumulators = []
+        self.posts = []
         self.next()
 
-    def add(self,analyzer):
+    def accumulator(self,analyzer):
+        """Add an analyzer that accumulates each temperature"""
         if not isinstance(analyzer, AnalyzerInterface):
             raise TypeError("{} is not an AnalyzerInterface".format(analyzer.__class__.__name__))
-        self.analyzers.append( analyzer )
+        self.accumulators.append( analyzer )
+
+    def post(self,analyzer):
+        """Add an analyzer that runs at analyze() time"""
+        if not isinstance(analyzer, AnalyzerInterface):
+            raise TypeError("{} is not an AnalyzerInterface".format(analyzer.__class__.__name__))
+        self.posts.append( analyzer )
 
     def next(self):
         self.ct = 0
-        for a in self.analyzers:
+        for a in self.accumulators:
+            a.next()
+        for a in self.posts:
             a.next()
 
     def print_stats(self):
         print("  pixels {}".format(self.ct))
-        for a in self.analyzers:
+        for a in self.accumulators:
             print("  from {}".format(a.__class__.__name__))
             a.print_stats()
 
-    def __call__(self, x, y, temp):
-        """To add a value for analysis"""
-        self.ct += 1
-        for a in self.analyzers:
-            a(x,y,temp)
+    def __call__(self, x=-1, y=-1, temp=0):
+        """To add a value for analysis (x,y,temp),
+        or to signal end-of-frame ()
+        """
+
+        if (x == -1):
+            for a in self.posts:
+                a.post()
+        else:
+            self.ct += 1
+            for a in self.accumulators:
+                a(x,y,temp)
 
 from map_range import map_range
 
@@ -62,6 +95,9 @@ class Histo(AnalyzerInterface):
             temp = self.high_value
         i = map_range( temp, self.low_value, self.high_value, 0, self.bucket_ct-1)
         self.buckets[int(i)] += 1
+
+    def post():
+        pass
 
     def value_at(self, i):
         return map_range( i, 0, self.bucket_ct, self.low_value, self.high_value) + self.bucket_width/2
@@ -88,4 +124,48 @@ class Histo(AnalyzerInterface):
             sys.stdout.write(",")
         print("]")
 
+class MinMax(AnalyzerInterface):
+    """Min & max"""
+    def __init__(self):
+        self.next()
+
+    def next(self):
+        self.low = 1000.0
+        self.high = 0.0
+
+    def __call__(self,x,y,temp):
+        if (self.low > temp):
+            self.low = temp
+        if (self.high < temp):
+            self.high = temp
+
+    def post():
+        pass
+
+    def print_stats(self):
+        print("minmax[{:f},{:f},]".format(self.low,self.high)) # PROCESSING
+
+class FirstHigh(AnalyzerInterface):
+    """Looks in the histo for the pattern:
+    ...XXXX..YY
+    And calls the first Y "first high"
+
+    def __init__(self):
+        self.next()
+
+    def next(self):
+        self.low = 1000.0
+        self.high = 0.0
+
+    def __call__(self,x,y,temp):
+        if (self.low > temp):
+            self.low = temp
+        if (self.high < temp):
+            self.high = temp
+
+    def post():
+        pass
+
+    def print_stats(self):
+        print("minmax[{:f},{:f},]".format(self.low,self.high)) # PROCESSING
 
